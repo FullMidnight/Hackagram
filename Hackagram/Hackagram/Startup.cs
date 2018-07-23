0d42d35da0d26b4eec08237a425f99400cdc93f4
Emmanuel Ferran <eferran@microsoft.com>
2018-07-23 14:32:26 -0700

    Added Hello World

---
+++ Hackagram/Hackagram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackagram.Startup))]
+namespace Hackagram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
