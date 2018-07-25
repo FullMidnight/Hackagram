fa0232fc2bc6a8a8ef431819d817085e02d8c411
MrHacker <MrHacker@LAPTOP-168FHI77>
2018-07-25 13:18:32 -0700

    Added a Question input to the answer Validation call

--- Hackagram/Hackagram/Controllers/HomeController.cs
+++ Hackagram/Hackagram/Controllers/HomeController.cs
@@ -92,6 +92,46 @@ namespace Hackagram.Controllers
             JsonResult r = new JsonResult();
             return new JsonResult();
         }
+        [HttpPost]
+        public JsonResult ValidateAnswer(Question question)
+        {
+
+            string userEmail = User.Identity.Name;
+            bool correct = false;
+            if (question.Excercise == "Hackagram" && question.QuestionNumber == 2)
+            {
+                //Check for HTML and Javascript
+                if (question.Answer.Contains("<script>") && question.Answer.Contains("</script>"))
+                {
+                    correct = true;
+                    //Add SQL call to insert  for questions answered.
+                    var qAnswered = new QuestionAnswered(question.Excercise, userEmail, question.QuestionNumber);
+                    adminContext.QuestionsAnswered.Add(qAnswered);
+                    adminContext.SaveChanges();
+                }
+            }
+            else
+            {
+                string answerFromDB = (from q in adminContext.Questions
+                                       where q.Excercise == question.Excercise && q.QuestionNumber == question.QuestionNumber
+                                       select q.Answer).First();
+
+                if (question.Answer == answerFromDB)
+                {
+                    correct = true;
+                    //Add SQL call to insert  for questions answered.
+                    var qAnswered = new QuestionAnswered(question.Excercise, userEmail, question.QuestionNumber);
+                    adminContext.QuestionsAnswered.Add(qAnswered);
+                    adminContext.SaveChanges();
+                }
+
+            }
+
+
+
+            JsonResult r = new JsonResult();
+            return new JsonResult();
+        }
 
         /// <summary>
         /// Send an OpenID Connect sign-in request.
